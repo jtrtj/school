52fd9997f6d6345e14a8552f268c6777373a8cf9
jtrtj <jt.r@live.com>
2018-07-10 14:48:30 -0600

    add nav to application html and pass test

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,8 +1,5 @@
-=begin
-As a user
-When I visit any page
-I see links to see a list of all students, or create a new student
 
+=begin
 As a user
 When I visit `/students`
 And I click on a student's name
@@ -19,13 +16,25 @@ describe 'user' do
 
       visit student_path(student_1)
       
-      click on 'All Students'
+      click_on 'All Students'
 
       expect(current_path).to eq(students_path)
 
-      click on 'New Student'
+      click_on 'New Student'
 
       expect(current_path).to eq(new_student_path)
     end
+
+    it "can click a students name to go to student's show page" do
+      student_1 = Student.create(name: 'kjh')
+      student_1 = Student.create(name: 'jhg')
+
+      visit students_path
+
+      click_on student_1.name
+
+      expect(current_path.to eq(student_path(student_1))
+    end
+
   end
 end
