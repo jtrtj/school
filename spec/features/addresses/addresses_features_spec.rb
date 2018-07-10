be6afe6530d4a626da96db472fbe8b53e8b1f460
jtrtj <jt.r@live.com>
2018-07-10 15:45:10 -0600

    write new test in student show to include addresses in show

--- spec/features/addresses/addresses_features_spec.rb
+++ spec/features/addresses/addresses_features_spec.rb
@@ -1,16 +1,3 @@
-=begin
-As a user
-When I visit `/students/:id/addresses/new`
-And I fill in description with a description (e.g. "Summer Address")
-And I fill in street with a street address
-And I fill in city with a city
-And I fill in state with a state
-And I fill in zip code with a zip code
-And I click submit
-I am taken to that student's show page
-And I see the description, street, city, state, and zip on that page
-=end
-
 require 'rails_helper'
 
 describe 'user' do
